f1c2cc0e45de8a9926036b361e0e9afb1d4d257b
Wolffoner <wolffoner@gmail.com>
2021-07-03 13:57:13 -0300

    tda hash/create

--- TDAs/TDA-Hash/Dev/hash.c
+++ TDAs/TDA-Hash/Dev/hash.c
@@ -2,14 +2,48 @@
 #include <stdlib.h>
 #include "hash.h"
 
+#define FACTOR_CARGA 0.75
+
+typedef struct nodo_hash{
+  char* clave;
+  void* valor;
+}nodo_hash_t;
+
 struct hash{
+  nodo_hash_t** tabla_hash;
+  size_t capacidad_tabla;
+  size_t cant_ocupadas;
+  hash_destruir_dato_t destructor;
 };
 
-
-hash_t* hash_crear(hash_destruir_dato_t destruir_elemento, size_t capacidad_inicial){
-  return NULL;
+size_t funcion_hash(char* clave, size_t capacidad_tabla){
+  return 0;
 }
 
+bool excede_factor_carga(size_t capacidad_tabla, size_t cant_ocupadas){
+  bool excedido = false;
+  double resultado = ((double)capacidad_tabla/cant_ocupadas);
+  if(resultado >= FACTOR_CARGA)
+    excedido = true;
+  return excedido;
+}
+
+hash_t* hash_crear(hash_destruir_dato_t destruir_elemento, size_t capacidad_inicial){
+  if(capacidad_inicial < 3)
+    capacidad_inicial = 3;
+  
+  hash_t* hash = calloc(1, sizeof(hash_t));
+  if(!hash)
+    return NULL;
+
+  hash->capacidad_tabla = capacidad_inicial;
+  hash->destructor = destruir_elemento;
+  hash->tabla_hash = calloc(1, (sizeof(nodo_hash_t)*hash->capacidad_tabla));
+  if(!hash->tabla_hash)
+    return NULL;
+
+  return hash;
+}
 
 int hash_insertar(hash_t* hash, const char* clave, void* elemento){
   return 0;
@@ -32,6 +66,8 @@ bool hash_contiene(hash_t* hash, const char* clave){
 }
 
 void hash_destruir(hash_t* hash){
+  free(hash->tabla_hash);
+  free(hash);
 }
 
 size_t hash_con_cada_clave(hash_t* hash, bool (*funcion)(hash_t* hash, const char* clave, void* aux), void* aux){
--- TDAs/TDA-Hash/Dev/hash_pruebas.c
+++ TDAs/TDA-Hash/Dev/hash_pruebas.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 #include "pa2mm.h"
 
+void CrearUnHash_ConCapacidadMenorATres_CreaUnHashConCapacidadTres(){
+  hash_t* tabla = NULL;
+  tabla = hash_crear(NULL, 0);
+  pa2m_afirmar(tabla, "Tabla de hash es creada con capacidad menor a 3");
+  hash_destruir(tabla);
+}
+
+void CrearUnHash_ConCapacidadMayorATres_CreaUnHashConEsaCapacidad(){
+  hash_t* tabla = NULL;
+  tabla = hash_crear(NULL, 10);
+  pa2m_afirmar(tabla, "Tabla de hash es creada con capacidad mayor a 3");
+  hash_destruir(tabla);
+}
+
+
 int main(){
+  pa2m_nuevo_grupo("Creacion de tabla de hash");
+  CrearUnHash_ConCapacidadMenorATres_CreaUnHashConCapacidadTres();
+  CrearUnHash_ConCapacidadMayorATres_CreaUnHashConEsaCapacidad();
   return pa2m_mostrar_reporte();
 }
